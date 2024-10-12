a65f84e2ba663ba0692e817239f67e9e35e79bd4
sourabh <sourabhjangid2002@gmail.com>
2024-10-12 20:18:21 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this based on your file structure
+    './public/index.html', // Include paths to your HTML or other template files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
